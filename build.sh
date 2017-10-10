22cd7ebd5b476e965f861bcee0c3835d68520cce
Madis Pink <madis.pink@gmail.com>
2017-10-10 23:11:28 +0300

    Remove junk from manifest
    Reduces size to 1415 bytes

--- build.sh
+++ build.sh
@@ -8,8 +8,14 @@ set -x
 #TODO ensure that ANDROID_HOME is set
 
 echo "Creating base AndroidManifest.xml"
-$ANDROID_HOME/build-tools/26.0.2/aapt p -M app/AndroidManifest.xml -I $ANDROID_HOME/platforms/android-26/android.jar -f -F build/base.apk
+$ANDROID_HOME/build-tools/26.0.2/aapt p -M app/AndroidManifest.xml -S app/res -I $ANDROID_HOME/platforms/android-26/android.jar -f -F build/base.apk
 unzip build/base.apk -d build/apk
+
+# Don't use the original manifest with all the generated junk, use the compiled xml from layout instead
+rm build/apk/AndroidManifest.xml
+rm build/apk/resources.arsc
+mv build/apk/res/layout/manifest.xml build/apk/AndroidManifest.xml
+rm -rf build/apk/res
 
 echo "Creating empty classes.dex"
 touch build/apk/classes.dex
@@ -17,7 +23,8 @@ touch build/apk/classes.dex
 echo "Creating unsigned archive"
 zip -j -r build/app-unsigned.apk build/apk
 
-KEYSTORE_PASS=android $ANDROID_HOME/build-tools/26.0.2/apksigner sign --v1-signing-enabled false --ks app/keystore.jks --out build/signed-release.apk --ks-pass env:KEYSTORE_PASS --ks-key-alias android build/app-unsigned.apk
+echo "Signing archive"
+KEYSTORE_PASS=android $ANDROID_HOME/build-tools/26.0.2/apksigner sign --v1-signing-enabled false --ks app/keystore.jks --out build/signed-release.apk --ks-pass env:KEYSTORE_PASS --ks-key-alias android --min-sdk-version 24 build/app-unsigned.apk
 
 set +x
 
